fae07e1028214984f79cbee2af42e65d9993a82a
tsr <82254860+tsr-on-github@users.noreply.github.com>
2021-11-30 23:37:46 +0900

    Let's gaming!

---
+++ gaming-cmd/gaming_cmd.bat
@@ -0,0 +1,17 @@
+@echo off
+chcp 65001
+echo ゲーミングコマンドプロンプト開始
+for /l %%I in (0,0,0) do (
+color 61
+color C9
+color 42
+color DA
+color 5E
+color 16
+color 9C
+color 24
+color AD
+color E5
+)
+
+exit /B
